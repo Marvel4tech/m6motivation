93a2a273542bf1565f064ace7272204f1cdc9f5e
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-01 00:21:10 +0100

    commit

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,20 +4,24 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
 
   return (
-    <header>
-        <nav>
-            <div>
+    <header className="">
+        <nav className=" max-w-6xl mx-auto flex justify-between items-center h-[90px]">
+            <div className=" border-b border-b-blue-600">
                 <Link to={'/'}>
-                    <FcIdea />
-                    <h2>m6motivation</h2>
+                    <div className=" flex flex-col items-center ">
+                        <FcIdea className=" text-4xl " />
+                        <h2 className=" font-mono font-bold">m6motivation</h2>
+                    </div>
                 </Link>
             </div>
-            <div>
-                <Link to={'/create'}>CREATE</Link>
-                <Link to={'/about'}>ABOUT</Link>
-                <Link to={'/feedback'}>FEEDBACK</Link>
-                <Link to={'/login'}>LOGIN</Link>
-                <Link to={'/login'}>SIGN UP</Link>
+            <div className=" flex text-sm font-medium items-center justify-center">
+                <Link className=" py-9 px-5 hover:bg-slate-200" to={'/create'}>CREATE</Link>
+                <Link className=" py-9 px-5 hover:bg-slate-200" to={'/about'}>ABOUT</Link>
+                <Link className=" py-9 px-5 hover:bg-slate-200" to={'/feedback'}>FEEDBACK</Link>
+                <Link className=" py-9 px-5 hover:bg-slate-200" to={'/login'}>LOGIN</Link>
+                <Link className=" py-3 px-6 border border-blue-800 rounded-full hover:bg-blue-800 hover:text-white" to={'/signup'}>
+                    SIGN UP
+                </Link>
             </div>
         </nav>
     </header>
