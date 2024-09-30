c76e45bdfbc7bce121f7082010a82b6b430b8504
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-30 17:46:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/pages/Home"
+import Create from "./components/pages/Create"
+import About from "./components/pages/About"
+import Feedback from "./components/pages/Feedback"
+import Login from "./components/pages/Login"
 
 
 function App() {
 
   return (
     <main className=" overflow-x-hidden">
-        
+        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
     </main>
   )
 }
