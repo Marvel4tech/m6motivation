2d555cd3bd67e0ea3700a659c235be1a2649967b
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-04 02:20:49 +0100

    commit

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import ReactQuill from "react-quill"
+import 'react-quill/dist/quill.snow.css';
+
 
 const Create = () => {
+
   return (
-    <div>Create</div>
+    <div className=' px-4 md:px-5 mt-8'>
+        <div className=" max-w-6xl mx-auto">
+            <form>
+                <input
+                  type="text"
+                  placeholder=" title"
+                />
+                <ReactQuill
+                  theme="snow"
+                />
+            </form>
+        </div>
+    </div>
   )
 }
 
