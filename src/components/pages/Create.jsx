2231525a9e1ec71e2f2d20b48ceba2a217637363
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-04 13:46:27 +0100

    commit

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -5,7 +5,8 @@ import 'react-quill/dist/quill.snow.css';
 
 const Create = () => {
   const [selectedCategories, setSelectedCategories] = useState([])
-  const [imagePreview, setImagePreview] = useState()
+  const [image, setImage] = useState(null)
+  const [imagePreview, setImagePreview] = useState(null)
 
   // Categories
   const categories = ['TRENDING', 'REVIEWS', 'LEAKS']
@@ -19,14 +20,25 @@ const Create = () => {
   }
 
   // Image Preview
-  const handleImagePreview = () => {
-    
-  }
+  const handleImagePreview = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+          setImage(file)
+          const reader = new FileReader()
+          reader.onload = () => {
+            setImagePreview(reader.result)
+          }
+          reader.readAsDataURL(file)
+      } else {
+        setImage(null)
+        setImagePreview(null)
+      }
+  };
 
   // Custom details for React Quills Text Editor - font, size, color, bg-color, and align options
   const sizeOptions = [
     { label: 'Small', value: 'small' },
-    { label: 'Normal', value: 'false' },
+    { label: 'Normal', value: '' },
     { label: 'Large', value: 'large' },
     { label: 'Huge', value: 'huge' },
   ]
@@ -75,9 +87,9 @@ const Create = () => {
   };
 
   return (
-    <div className=' px-4 md:px-5 mt-8'>
+    <div className=' px-4 md:px-5 my-8'>
         <div className=" max-w-6xl mx-auto">
-            <form>
+            <form className=" space-y-6">
                 <input
                   className=" border-[1.5px] border-gray-300 w-full py-3 px-4"
                   type="text"
@@ -92,27 +104,35 @@ const Create = () => {
                   {categories.map((category) =>(
                     <label className=" text-sm py-2" key={category}>
                       <input 
-                        type="radio"
+                        type="checkbox"
                         checked={selectedCategories.includes(category)}
                         onChange={() => handleCheckBoxChange(category)}
+                        className=" mr-2"
                       />
                       {category.charAt(0).toUpperCase() + category.slice(1)}
                     </label>
                   ))} 
                 </div>
-                <div className=" bg-red-500">
+                {imagePreview && (
+                  <div className=" bg-red-500">
                     <img 
                       src={imagePreview} 
                       alt="Preview"
                       className=" max-h-[300px] w-[100%] object-cover"
                     />
-                </div>
+                  </div>
+                )}
                 <input 
                   type="file"
-                  accept="image/"
-                  onChange={handleImageChange}
+                  accept="image/*"
+                  onChange={handleImagePreview}
                   required
+                  className=" border w-full border-gray-300"
                 />
+                <button type="submit" className=" px-6 py-2 font-semibold bg-green-500 hover:bg-green-600 text-white transition-all 
+                duration-300">
+                  Save
+                </button>
             </form>
         </div>
     </div>
