371d05e957c61cf95c57d8d5b9cd203954165ce8
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-04 02:35:00 +0100

    react quill text editor is added with more toolbars

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -4,6 +4,57 @@ import 'react-quill/dist/quill.snow.css';
 
 const Create = () => {
 
+  // Custom details for React Quills Text Editor - font, size, color, bg-color, and align options
+  const sizeOptions = [
+    { label: 'Small', value: 'small' },
+    { label: 'Normal', value: 'false' },
+    { label: 'Large', value: 'large' },
+    { label: 'Huge', value: 'huge' },
+  ]
+
+  const colorOptions = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Yellow', value: 'yellow' },
+    { label: 'Custom', value: '#000000' }
+  ]
+
+  const backgroundColorOptions = [
+    { label: 'White', value: 'white' },
+    { label: 'Black', value: 'black' },
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Yellow', value: 'yellow' },
+    { label: 'Transparent', value: 'transparent'},
+    { label: 'Custom', value: '#ffffff' }
+  ]
+
+  const alignOptions = [
+    { label: 'Left', value: 'left' },
+    { label: 'Center', value: 'center' },
+    { label: 'Right', value: 'right' },
+    { label: 'Justify', value: 'justify' },
+  ]
+
+   // Toolbar configurations
+  const modules = {
+    toolbar: [
+      [{'size': sizeOptions.map(size => size.value) }],
+      [{ 'header': '1' }, { 'header': '2' }, { 'header': [3, 4, 5, 6] }, {'header': false }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'color': colorOptions.map(color => color.value) }, { 'background': backgroundColorOptions
+          .map(backgroundColor => backgroundColor.value) }
+      ],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'align': alignOptions.map(align => align.value) }],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+
   return (
     <div className=' px-4 md:px-5 mt-8'>
         <div className=" max-w-6xl mx-auto">
@@ -14,6 +65,7 @@ const Create = () => {
                 />
                 <ReactQuill
                   theme="snow"
+                  modules={modules}
                 />
             </form>
         </div>
