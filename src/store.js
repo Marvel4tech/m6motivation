32f3f491a4d6cf1bd58f0d004b9cf0130003e30b
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-05 02:41:15 +0100

    commit

--- src/store.js
+++ src/store.js
@@ -1,7 +1,18 @@
 import { create } from "zustand";
+import { db, auth, storage } from "./firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { uploadBytes, ref } from "firebase/storage";
 
 const useStore = create((set) => ({
-
+    posts: [],
+    fetchPosts: async () => {
+        const querySnapshot = await getDocs(collection(db, 'blogPosts'))
+        const posts = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+        set({ posts })
+    },
+    createPosts: async ( title, content,  ) => {
+        const date = new Date().toISOString()
+    }
 }));
 
 export default useStore;
