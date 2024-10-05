16b740d484928ed6ea10698be3807009dd1da4e9
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-05 22:44:54 +0100

    commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Trending from '../homeCategories/Trending'
 import Reviews from '../homeCategories/Reviews'
 import LatestPosts from '../homeCategories/LatestPosts'
+import useStore from '../../store'
 
 const Home = () => {
+  const { posts, fetchPosts } = useStore()
+
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
+
   return (
     <div className=' px-4 md:px-5 mt-8'>
         <div className=' max-w-6xl mx-auto flex flex-col-reverse md:flex-col gap-10'>
