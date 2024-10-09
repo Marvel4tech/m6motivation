a25fe92ecf3ec0e31c0b112ce34ad0c06498b48d
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-09 15:34:29 +0100

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-        <div>
-            footer
+    <footer className=" px-4 md:px-5 border-t border-t-yellow-500 mt-14">
+        <div className=' max-w-6xl mx-auto py-10 grid md:grid-cols-3'>
+            <div>
+                <div className=" flex flex-col items-center ">
+                    <FcIdea className=" text-4xl " />
+                    <h2 className=" font-mono font-bold">m6motivation</h2>
+                </div>
+            </div>
+            <div>
+                col 2
+            </div>
+            <div>
+                col 3
+            </div>
         </div>
     </footer>
   )
