362e0b7b18deddb3aab6e3dc3607c59b5beeeaa7
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-09 13:37:50 +0100

    commit

--- src/components/homeCategories/Comparisons.jsx
+++ src/components/homeCategories/Comparisons.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
-const Comparisons = () => {
+const Comparisons = ({ posts }) => {
+
   return (
     <div className=' flex-1'>
         <div>
             <h2 className=' bg-gray-800 text-white font-bold px-6 py-2 w-fit'>
                 COMPARISONS
             </h2>
+            <ul className=' mt-5 space-y-6'>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <img 
+                            src={post.imageUrl} 
+                            alt={post.title} 
+                            className=' h-[24rem] w-full object-cover'
+                        />
+                        <h3 className=' font-semibold text-xl md:text-2xl mt-2 leading-snug'>{post.title}</h3>
+                    </li>
+                ))}
+            </ul>
+            <button className=' bg-gray-500 hover:bg-gray-600 w-full text-white py-2 rounded-md mt-5'>
+              More Comparisons
+            </button>
         </div>
     </div>
   )
