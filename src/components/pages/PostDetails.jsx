b92a4521f39566c0b2f9d4a0e1a161ce409c36e6
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-13 14:13:40 +0100

    commit

--- src/components/pages/PostDetails.jsx
+++ src/components/pages/PostDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import useStore from "../../store"
 import { useEffect } from "react"
+import DOMPurify from "dompurify"
 
 
 
@@ -9,17 +10,42 @@ const PostDetails = () => {
     const { singlePost, fetchSinglePost } = useStore()
 
     useEffect(() => {
-        fetchSinglePost(id)
-    }, [id])
+        if (!singlePost || singlePost.id !== id) {
+          fetchSinglePost(id)
+        }
+      }, [id, singlePost])
 
     if (!singlePost) {
         return <div>Post not found</div>
     }
 
+    const date = new Date(singlePost.date);
+    const month = date.toLocaleString('default', { month: 'short' });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const formattedDate = `${month} ${day}, ${year}`;
+
   return (
-     <div className=" mt-32">
-        <div className="">
-            <h1 className=" text-red-700">{singlePost.title}</h1>
+     <div className=' px-4 md:px-5 mt-32'>
+        <div className=" max-w-6xl mx-auto flex gap-10">
+            <div className=" flex-[2]">
+                <h1 className="">
+                    {singlePost.title}
+                </h1>
+                <span>{formattedDate}</span>
+                <img 
+                    src={singlePost.imageUrl}
+                    alt={singlePost.title}
+                />
+                <p 
+                    dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(
+                        singlePost.content.replace(/<(?!p|span).*?>|<br\/?>/g, '')
+                    )}}
+                />
+            </div>
+            <div className=" border flex-[1]">
+                Advertisment
+            </div>
         </div>
     </div>
   )
